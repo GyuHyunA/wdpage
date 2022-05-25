59726c9718f493e58e655c3bf622d5be89effbc2
leemit <ghahn97@gmail.com>
2022-05-26 02:39:02 +0900

    fix modal(#1)

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-let open = document.querySelector('')
+let modal = document.querySelector("#ismodal");
+let momodal = document.querySelector("#mo-modal");
+let outmodal = document.querySelector("#out-modal");
+
+momodal.addEventListener("click", open);
+outmodal.addEventListener("click", close);
+// console.log(open);
+
+// window.addEventListener('click', close)
+
+function close() {
+  modal.classList.remove("active");
+}
+
+function open() {
+  modal.classList.add("active");
+  // console.log('hi')
+}
