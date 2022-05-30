ef848b151035e419513d6057e63c749fc758b542
leemit <ghahn97@gmail.com>
2022-05-31 01:19:37 +0900

    fix modal

--- js/main.js
+++ js/main.js
@@ -1,24 +1,41 @@
-let modal = document.querySelector("#ismodal");
+let moismodal = document.querySelector("#mo-ismodal");
+let prismodal = document.querySelector("#pr-ismodal");
+let afismodal = document.querySelector("#af-ismodal");
+
 let momodal = document.querySelector("#mo-modal");
 let prmodal = document.querySelector("#pr-modal");
 let afmodal = document.querySelector("#af-modal");
 
-let outmodal = document.querySelector("#out-modal");
+let mooutmodal = document.querySelector("#mo-out-modal");
+let proutmodal = document.querySelector("#pr-out-modal");
+let afoutmodal = document.querySelector("#af-out-modal");
 
-momodal.addEventListener("click", open);
-prmodal.addEventListener("click", open);
-afmodal.addEventListener("click", open);
+momodal.addEventListener("click", moopen);
+prmodal.addEventListener("click", propen);
+afmodal.addEventListener("click", afopen);
 
-outmodal.addEventListener("click", close);
-// console.log(open);
+proutmodal.addEventListener("click", prclose);
+mooutmodal.addEventListener("click", moclose);
+afoutmodal.addEventListener("click", afclose);
 
-// window.addEventListener('click', close)
 
-function close() {
-  modal.classList.remove("active");
+function moclose() {
+  moismodal.classList.remove("active");
 }
 
-function open() {
-  modal.classList.add("active");
-  // console.log('hi')
+function prclose() {
+  prismodal.classList.remove("active");
+}
+function afclose() {
+  afismodal.classList.remove("active");
+}
+
+function moopen() {
+  moismodal.classList.add("active");
+}
+function propen() {
+  prismodal.classList.add("active");
+}
+function afopen() {
+  afismodal.classList.add("active");
 }
