240edeb6e5fe06bb74cedf47b6320f20f20cb3fa
leemit <ghahn97@gmail.com>
2022-05-31 01:04:21 +0900

    add modal

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 let modal = document.querySelector("#ismodal");
 let momodal = document.querySelector("#mo-modal");
+let prmodal = document.querySelector("#pr-modal");
+let afmodal = document.querySelector("#af-modal");
+
 let outmodal = document.querySelector("#out-modal");
 
 momodal.addEventListener("click", open);
+prmodal.addEventListener("click", open);
+afmodal.addEventListener("click", open);
+
 outmodal.addEventListener("click", close);
 // console.log(open);
 
