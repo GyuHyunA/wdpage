d30e2e699214c5de78781c7b3f84a787f3ca7090
leemit <ghahn97@gmail.com>
2022-06-15 11:16:42 +0900

    modal button fix

--- js/main.js
+++ js/main.js
@@ -1,3 +1,38 @@
+// filter
+let ci = document.querySelector(".circle");
+let fi = document.querySelector(".fi");
+let bt = document.querySelector(".bt");
+let fi2 = document.querySelector(".fi2");
+let bt2 = document.querySelector(".bt2");
+let fi3 = document.querySelector(".fi3");
+let bt3 = document.querySelector(".bt3");
+
+function ovmo() {
+  fi.classList.add("fi-active");
+  bt.classList.add("bt-active");
+}
+function outmo() {
+  fi.classList.remove("fi-active");
+  bt.classList.remove("bt-active");
+}
+function ovmo2() {
+  fi2.classList.add("fi-active");
+  bt2.classList.add("bt-active");
+}
+function outmo2() {
+  fi2.classList.remove("fi-active");
+  bt2.classList.remove("bt-active");
+}
+function ovmo3() {
+  fi3.classList.add("fi-active");
+  bt3.classList.add("bt-active");
+}
+function outmo3() {
+  fi3.classList.remove("fi-active");
+  bt3.classList.remove("bt-active");
+}
+
+// modal
 let moismodal = document.querySelector("#mo-ismodal");
 let prismodal = document.querySelector("#pr-ismodal");
 let afismodal = document.querySelector("#af-ismodal");
@@ -17,7 +52,6 @@ afmodal.addEventListener("click", afopen);
 proutmodal.addEventListener("click", prclose);
 mooutmodal.addEventListener("click", moclose);
 afoutmodal.addEventListener("click", afclose);
-
 
 function moclose() {
   moismodal.classList.remove("active");
